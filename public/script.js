f8bf4ed5023143f23e542b98621aa69063249b55
Andrea Astorga <andrea.astorga95@gmail.com>
2021-03-16 17:31:55 +0100

    Adding uuid and socket.io

---
+++ public/script.js
@@ -0,0 +1,35 @@
+// Js frontend
+
+const socket = io('/');
+
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+    // variable recieves the video
+})
+
+socket.emit('join-room', ROOM_ID);
+
+socket.on('user-connected', () => {
+    connectToNewUser();
+})
+
+const connectToNewUser = () => {
+    console.log('new user');
+}
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video)
+}
