8e8d103d44f25f1edafc0d01187fc29b58fa7ccd
Andrea Astorga <andrea.astorga95@gmail.com>
2021-03-24 23:13:08 +0100

    Added chat, mute and  video functionalities

--- public/script.js
+++ public/script.js
@@ -1,4 +1,8 @@
 // Js frontend
+// socket.emit =>>> will send
+// socket.on =>>>> will receive
+
+// const { text } = require("express");
 
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
@@ -17,7 +21,7 @@ let peer = new Peer(undefined, {
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -34,6 +38,33 @@ navigator.mediaDevices.getUserMedia({
     socket.on('user-connected', (userId) => {
         connectToNewUser(userId, stream)
     })
+
+    const text = document.getElementById('chat-message')
+
+    document.addEventListener('keydown', event => {
+        const keyName = event.key
+        if (keyName == 'Enter' && text.value.length !== 0) {
+            socket.emit('message', text.value);
+            text.value = '';
+        }
+    })
+
+    socket.on('createMessage', message => {
+        console.log(message)
+        const ulMessages = document.getElementById('ul-messages');
+        const newLi = document.createElement('li');
+        const userSendingMsg = document.createElement('B')
+        userSendingMsg.innerHTML += 'user'
+        const breakSpace = document.createElement('br')
+        newLi.appendChild(userSendingMsg);
+        newLi.appendChild(breakSpace);
+        newLi.appendChild(document.createTextNode(`${message}`));
+        newLi.setAttribute('class', 'message');
+        ulMessages.appendChild(newLi)
+        // ulMessages.append(`<li class="message"><b>user</b><br/>${message}</li>`)
+        scrollToBottom()
+    })
+
 
 })
 
@@ -60,4 +91,68 @@ const addVideoStream = (video, stream) => {
         video.play();
     })
     videoGrid.append(video)
+}
+
+const scrollToBottom = () => {
+    let chatWindow = document.querySelector('.main-chat-window');
+    chatWindow.scrollTop = chatWindow.scrollHeight    
+}
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+        <i class="fas fa-microphone"></i>
+        <span>Mute</span>
+    `
+
+    document.querySelector('.main-mute-button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+        <i class="unmute fas fa-microphone-slash"></i>
+        <span>Unmute</span>
+    `
+
+    document.querySelector('.main-mute-button').innerHTML = html;
+}
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true
+    }
+}
+
+const setStopVideo = () => {
+    const html = `
+        <i class="fas fa-video"></i>
+        <span>Stop Video</span>
+    `
+
+    document.querySelector('.main-video-button').innerHTML = html
+}
+
+const setPlayVideo = () => {
+    const html = `
+        <i class="stop fas fa-video-slash"></i>
+        <span>Stop Video</span>
+    `
+
+    document.querySelector('.main-video-button').innerHTML = html
 }
